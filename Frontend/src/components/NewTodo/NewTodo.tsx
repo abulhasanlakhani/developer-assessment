9f2f90555a35182ff83dff1604d956380aa33a25
Abul Hasan Lakhani <abulhasanlakhani@gmail.com>
2022-02-27 13:42:44 +1300

    Converted remaining js files to typescript

--- Frontend/src/components/NewTodo/NewTodo.tsx
+++ Frontend/src/components/NewTodo/NewTodo.tsx
@@ -5,8 +5,8 @@ import axios from 'axios'
 
 const NewTodo = ({ todoItems, setTodoItems }) => {
   const API_URL = 'https://localhost:5001/api/todoitems'
-  const descriptionRef = useRef()
-  const errRef = useRef()
+  const descriptionRef = useRef<HTMLInputElement>()
+  const errRef = useRef<HTMLParagraphElement>()
 
   const [description, setDescription] = useState('')
   const [validDescription, setValidDescription] = useState(false)
@@ -14,7 +14,6 @@ const NewTodo = ({ todoItems, setTodoItems }) => {
   const [errMsg, setErrMsg] = useState('')
 
   const validateDescription = () => {
-    
     if (description === '') {
       setErrMsg('Description is required')
       setValidDescription(false)
@@ -44,8 +43,7 @@ const NewTodo = ({ todoItems, setTodoItems }) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
 
-    if (!validateDescription())
-      return
+    if (!validateDescription()) return
 
     try {
       const response = await axios.post(API_URL, {
@@ -101,7 +99,7 @@ const NewTodo = ({ todoItems, setTodoItems }) => {
       </Form.Group>
       <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
         <Stack direction="horizontal" gap={2}>
-          <Button variant="primary" onClick={handleSubmit} name='btnAddTodoItem'>
+          <Button variant="primary" onClick={handleSubmit} name="btnAddTodoItem">
             Add Item
           </Button>
           <Button variant="secondary" onClick={handleClear}>
---
+++ Frontend/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Button, Table } from 'react-bootstrap'
+import TodoItem from '../TodoItem/TodoItem'
+
+const TodoList = ({ items, handleMarkAsComplete }) => {
+  return items.length ? (
+    <>
+      <h1>
+        Showing {items.length} Item(s){' '}
+        <Button variant="primary" className="pull-right">
+          Refresh
+        </Button>
+      </h1>
+
+      <Table role="table" aria-label="todoList-table" striped bordered hover>
+        <thead>
+          <tr role="row">
+            <th role="columnheader">Id</th>
+            <th role="columnheader">Description</th>
+            <th role="columnheader">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <TodoItem key={index} item={item} handleMarkAsComplete={handleMarkAsComplete} />
+          ))}
+        </tbody>
+      </Table>
+    </>
+  ) : null
+}
+
+export default TodoList
